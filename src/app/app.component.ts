6b558e25218c0bea2150d6313122d26014396bbd
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2024-11-25 17:44:39 +0100

    refactor(project): migrate to angular
    Add tailwind.
    
    Closes: #17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
 })
 export class AppComponent {
   title = 'webfolio';
