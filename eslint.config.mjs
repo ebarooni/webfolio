12ad599547eaa1673ec2cc0091fd3a002dca65c5
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2024-11-28 18:21:42 +0100

    refactor(project): migrate to angular
    Add eslint without angular eslint.

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+import eb from "@ebarooni/eslint-config/angular-recommended-type-checked";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(...eb, {
+  languageOptions: {
+    parserOptions: {
+      project: ["**/tsconfig*.json"],
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+});
