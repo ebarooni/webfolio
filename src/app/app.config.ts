f19cbe511c98b0df716504696e146ce1960df77e
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2024-12-03 23:13:16 +0100

    refactor(project): migrate to angular
    Implement logging service.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideLogService } from './services/log/log.service.provider';
+import { IS_DEV_MODE } from './tokens/is-dev-mode';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideLogService(),
+    { provide: IS_DEV_MODE, useValue: isDevMode() },
   ],
 };
---
+++ src/app/tokens/is-dev-mode.ts
@@ -0,0 +1,9 @@
+import { InjectionToken } from '@angular/core';
+
+export const IS_DEV_MODE = new InjectionToken<boolean>(
+  'Returns true if running in development',
+  {
+    providedIn: 'root',
+    factory: () => false,
+  },
+);
