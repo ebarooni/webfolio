f69987f16dec47f986cdd7a51085427d1fa1c8a8
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2024-12-03 23:15:12 +0100

    refactor(project): migrate to angular
    Implement base layout component.

---
+++ src/app/layouts/base-layout/base-layout.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { BaseLayoutComponent } from './base-layout.component';
+import { appConfig } from '../../app.config';
+
+describe('BaseLayoutComponent', () => {
+  let component: BaseLayoutComponent;
+  let fixture: ComponentFixture<BaseLayoutComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule(
+      Object.assign({}, appConfig, {
+        imports: [BaseLayoutComponent],
+      }),
+    ).compileComponents();
+
+    fixture = TestBed.createComponent(BaseLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
