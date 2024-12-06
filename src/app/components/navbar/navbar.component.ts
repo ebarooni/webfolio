b339cb1ecb0d8f955b66a17de0c39d1ad4f5f240
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2024-12-06 23:58:15 +0100

    refactor(project): migrate to angular
    Implement desktop navbar.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,36 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  input,
+  output,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NavigationHelper } from '../../extends/navigation-helper';
+import { BaseNavigationComponent } from '../../extends/base-navigation.component';
 import { Theme } from 'daisyui';
 import { DataThemeDirective } from '../../directives/data-theme/data-theme.directive';
+import { NgClass } from '@angular/common';
+import { LogService } from '../../services/log/log.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive, DataThemeDirective],
+  imports: [RouterLink, RouterLinkActive, DataThemeDirective, NgClass],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent extends NavigationHelper {
-  @Output() themeChanged = new EventEmitter<Theme>();
+export class NavbarComponent extends BaseNavigationComponent {
+  @ViewChild('navbarDiv')
+  private readonly navbarDiv?: ElementRef<HTMLDivElement>;
+  public readonly selectedTheme = input.required<Theme>({ alias: 'theme' });
+  public readonly version = input<string | undefined>(undefined);
+  public readonly themeChanged = output<Theme>();
+
+  constructor(renderer: Renderer2, logService: LogService) {
+    super(renderer, logService);
+  }
+
+  @HostListener('document:scroll') override onDocumentScroll() {
+    super.onDocumentScroll(this.navbarDiv);
+  }
 }
