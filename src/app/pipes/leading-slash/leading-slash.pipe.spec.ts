b55248586dfe49005241b077ab9ac0d95214dd8f
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-12-18 22:31:18 +0100

    feat(navbar): make navbar component mobile friendly
    Remove leading slash from routes.

---
+++ src/app/pipes/leading-slash/leading-slash.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { LeadingSlashPipe } from './leading-slash.pipe';
+
+describe('LeadingSlashPipe', () => {
+  it('create an instance', () => {
+    const pipe = new LeadingSlashPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
