573b6d984d66462e2a7f6dcf9259547530f571ae
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-12-31 20:58:10 +0100

    feat(contact): implement contact page
    Add form controls.

--- src/app/pages/contact/contact-form/contact-form.component.ts
+++ src/app/pages/contact/contact-form/contact-form.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './contact-form.component.html',
 })
-export class ContactFormComponent {}
+export class ContactFormComponent {
+  readonly contactForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+    ]),
+  });
+
+  submit() {
+    console.log(this.contactForm.value);
+    this.contactForm.reset({
+      name: '',
+      email: '',
+      message: '',
+    });
+  }
+}
