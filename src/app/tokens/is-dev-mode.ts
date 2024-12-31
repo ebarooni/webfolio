d1875d3ceb36f63c0da5bfa19f9ca0cc7cfe6726
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-12-31 18:12:08 +0100

    feat(contact): implement contact page
    Remove providing dev mode token in root.

--- src/app/tokens/is-dev-mode.ts
+++ src/app/tokens/is-dev-mode.ts
@@ -3,7 +3,6 @@ import { InjectionToken } from '@angular/core';
 export const IS_DEV_MODE = new InjectionToken<boolean>(
   'Returns true if running in development',
   {
-    providedIn: 'root',
     factory: () => false,
   },
 );
