d462dec35d0896bb80792cb66e993c7972e7dfa5
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-01-01 12:57:55 +0100

    feat(build-info): implement build info page
    Create deployment detail component.

---
+++ src/app/pages/build-info/deployment-details/deployment-details.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deployment-details',
+  templateUrl: './deployment-details.component.html',
+})
+export class DeploymentDetailsComponent {}
