fe247cd457a5a8ea18b70623016123df3fe10f76
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-01-04 23:00:21 +0100

    feat(build-info): implement build info page
    Complete build info page.

---
+++ src/app/pages/build-info/build-info.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { BuildInfoComponent } from './build-info.component';
+
+describe('BuildInfoComponent', () => {
+  let component: BuildInfoComponent;
+  let fixture: ComponentFixture<BuildInfoComponent>;
+
+  beforeEach(waitForAsync(() => {
+    void TestBed.configureTestingModule({
+      imports: [BuildInfoComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BuildInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
