6fa5b3dfc400a2421be0926610b6ac6b4579e16e
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-01-04 22:58:51 +0100

    feat(build-info): implement build info page
    Transform dependencies to table rows.

--- src/app/pages/build-info/dependencies/dependencies.component.ts
+++ src/app/pages/build-info/dependencies/dependencies.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-dependencies',
   templateUrl: './dependencies.component.html',
 })
-export class DependenciesComponent {}
+export class DependenciesComponent {
+  public readonly title = input<string>('Dependencies');
+  public readonly dependencies = input.required({
+    transform: (deps: Record<string, string>) => this.convertDepsToArray(deps),
+  });
+
+  private convertDepsToArray(
+    deps: Record<string, string>,
+  ): { name: string; version: string }[] {
+    return Object.entries(deps).map(([key, value]) => ({
+      name: key,
+      version: value,
+    }));
+  }
+}
