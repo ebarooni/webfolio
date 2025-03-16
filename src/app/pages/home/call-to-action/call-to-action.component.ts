d8636e4ccc521590da84631305c77f1d033fe3e7
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-03-16 15:20:48 +0100

    feat(about): implement about page
    Remove deprecated components.

--- src/app/pages/home/call-to-action/call-to-action.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-call-to-action',
-  templateUrl: './call-to-action.component.html',
-})
-export class CallToActionComponent {}
