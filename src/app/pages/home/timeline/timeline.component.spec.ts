6d7eaaa94dfbe168dd4d8467b2825768729f3bf0
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-03-28 07:36:41 +0100

    feat(about): implement about page
    Implement timeline in home component.

---
+++ src/app/pages/home/timeline/timeline.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { TimelineComponent } from './timeline.component';
+
+describe('TimelineComponent', () => {
+  let component: TimelineComponent;
+  let fixture: ComponentFixture<TimelineComponent>;
+
+  beforeEach(waitForAsync(() => {
+    void TestBed.configureTestingModule({
+      imports: [TimelineComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TimelineComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
