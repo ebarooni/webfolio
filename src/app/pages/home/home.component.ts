9055336d5dfe8e2618fcb0dae8ad9b0cb3853fc0
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-04-01 23:25:31 +0200

    feat(about): implement about page
    Fix dayjs date on safari.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -19,7 +19,8 @@ import dayjs from 'dayjs';
   templateUrl: './home.component.html',
 })
 export class HomeComponent extends BaseRoutingComponent {
-  readonly yearsOfExperience = dayjs().diff(dayjs('01-01-2021'), 'year');
+  // safari requires dd/mm/yyyy instead of dd-mm-yyyy
+  readonly yearsOfExperience = dayjs().diff(dayjs('01-01-2021'.replace(/-/g, '/')), 'year');
   readonly window = signal(window);
   readonly showScrollIndicator = signal(this.isDocumentScrolling());
 
