91918f754fb3c55d18c3b9b619dc1d40d394627d
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-04-10 13:57:30 +0200

    feat(contact): sanitize texts before submitting
    Change project badge colors.

--- src/app/pages/projects/showcase/showcase.component.ts
+++ src/app/pages/projects/showcase/showcase.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { OpenComponent } from '../../../svg-components/open/open.component';
 import { ProjectItemComponent } from './project-item/project-item.component';
 
 @Component({
+  imports: [ProjectItemComponent, OpenComponent],
   selector: 'app-showcase',
   templateUrl: './showcase.component.html',
-  imports: [ProjectItemComponent],
 })
 export class ShowcaseComponent {}
