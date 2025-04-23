c43958abbb0c789a0e46489c5ce830079f8dcc1f
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-04-23 22:49:34 +0200

    feat(blog): implement blog page
    Create empty blog component.

---
+++ src/app/pages/blog/blog.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { BlogComponent } from './blog.component';
+
+describe('BlogComponent', () => {
+  let component: BlogComponent;
+  let fixture: ComponentFixture<BlogComponent>;
+
+  beforeEach(waitForAsync(() => {
+    void TestBed.configureTestingModule({
+      imports: [BlogComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BlogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
