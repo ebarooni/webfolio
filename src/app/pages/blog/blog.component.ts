089cabf12db4243d90f66e1e969e4353623ab20c
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-06-30 11:43:14 +0200

    feat(blog): implement blog page
    Add dummy blog post.

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,9 +1,22 @@
+import { BaseLayoutComponent } from '../../layouts/base-layout/base-layout.component';
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { HeroComponent } from '../../components/hero/hero.component';
 
 @Component({
-  imports: [],
+  imports: [BaseLayoutComponent, DatePipe, HeroComponent],
   selector: 'app-blog',
-  styleUrl: './blog.component.scss',
   templateUrl: './blog.component.html',
 })
-export class BlogComponent {}
+export class BlogComponent {
+  posts = [
+    {
+      date: new Date('2024-12-01'),
+      id: 1,
+      readTime: 5,
+      summary:
+        'A breakdown of Angular Signals and how to use them effectively.',
+      title: 'Understanding Angular Signals',
+    },
+  ];
+}
