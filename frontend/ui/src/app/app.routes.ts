2e096de5a764672bdd5babf71a692438ed6c988a
Ehsan Barooni <mail@barooni.dev>
2026-02-18 23:26:02 +0100

    feat: pass screen data from routes

--- frontend/ui/src/app/app.routes.ts
+++ frontend/ui/src/app/app.routes.ts
@@ -3,52 +3,76 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    loadComponent: () =>
-      import('./pages/access-denied/access-denied.component').then(
-        (m) => m.AccessDeniedComponent,
-      ),
     path: Route.ACCESS_DENIED,
+    loadComponent: () =>
+      import('./access-denied/access-denied').then(
+        (m) => m.AccessDenied,
+      ),
     title: 'Access denied',
+    data: { 
+      page: Route.ACCESS_DENIED,
+    },
   },
   {
+    path: '',
     loadComponent: () =>
-      import('./pages/blog/blog.component').then((m) => m.BlogComponent),
-    path: Route.BLOG,
-    title: 'Blog | Ehsan Barooni',
-  },
-  {
-    loadComponent: () =>
-      import('./pages/projects/projects.component').then(
-        (m) => m.ProjectsComponent,
+      import('./app-shell/app-shell').then(
+        (m) => m.AppShell,
       ),
-    path: Route.PROJECTS,
-    title: 'Projects | Ehsan Barooni',
-  },
-  {
-    loadComponent: () =>
-      import('./pages/build-info/build-info.component').then(
-        (m) => m.BuildInfoComponent,
-      ),
-    path: Route.BUILD_INFO,
-    title: 'Build Info | Ehsan Barooni',
-  },
-  {
-    loadComponent: () =>
-      import('./pages/contact/contact.component').then(
-        (m) => m.ContactComponent,
-      ),
-    path: Route.CONTACT,
-    title: 'Contact | Ehsan Barooni',
-  },
-  {
-    loadComponent: () =>
-      import('./pages/home/home.component').then((m) => m.HomeComponent),
-    path: Route.HOME,
-    title: 'Ehsan Barooni | Full-Stack Software Engineer',
-  },
-  {
-    path: '**',
-    pathMatch: 'full',
-    redirectTo: Route.HOME,
+    children: [
+      {
+        path: Route.HOME,
+        loadComponent: () =>
+          import('./home/home').then((m) => m.HomeComponent),
+        title: 'Ehsan Barooni | Full-Stack Software Engineer',
+        data: { 
+          footerBgClass: 'bg-base-200',
+          page: Route.HOME,
+        },
+      },
+      {
+        path: Route.BLOG,
+        loadComponent: () =>
+          import('./blog/blog').then((m) => m.BlogComponent),
+        title: 'Blog | Ehsan Barooni',
+        data: { 
+          page: Route.BLOG,
+         },
+      },
+      {
+        path: Route.PROJECTS,
+        loadComponent: () =>
+          import('./projects/projects').then(
+            (m) => m.ProjectsComponent,
+          ),
+        title: 'Projects | Ehsan Barooni',
+        data: { 
+          page: Route.PROJECTS,
+         },
+      },
+      {
+        path: Route.CONTACT,
+        loadComponent: () =>
+          import('./contact/contact').then(
+            (m) => m.ContactComponent,
+          ),
+        title: 'Contact | Ehsan Barooni',
+        data: { 
+          page: Route.CONTACT,
+         },
+      },
+      {
+        path: Route.BUILD_INFO,
+        loadComponent: () =>
+          import('./build-info/build-info').then(
+            (m) => m.BuildInfoComponent,
+          ),
+        title: 'Build Info | Ehsan Barooni',
+        data: { 
+          page: Route.BUILD_INFO,
+         },
+      },
+      { path: '**', redirectTo: Route.HOME, pathMatch: 'full' },
+    ],
   },
 ];
