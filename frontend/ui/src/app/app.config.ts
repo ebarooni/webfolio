9a85094d407e40c1c32aaffcaae06f8d0a92321a
Ehsan Barooni <mail@barooni.dev>
2026-03-05 22:44:28 +0100

    refactor: remove dev mode token

--- frontend/ui/src/app/app.config.ts
+++ frontend/ui/src/app/app.config.ts
@@ -1,18 +1,15 @@
 import {
   ApplicationConfig,
-  isDevMode,
   provideAppInitializer,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
-import { IS_DEV_MODE } from './config/is-dev-mode';
 import { initializeApp } from './initialize-app';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    { provide: IS_DEV_MODE, useValue: isDevMode() },
     provideAppInitializer(initializeApp),
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
